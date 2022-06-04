dd23e0f5e564a256fe716ed25cbadc477a5eb86f
roymm <royamunoz@protonmail.com>
2022-06-03 21:16:11 -0600

    Adds timestamps to the User model

--- models/users.js
+++ models/users.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema({
   password: {
     type: String,
     required: true,
-  },
-});
+  }
+}, {timestamps: true});
 
 module.exports = mongoose.model("User", userSchema);
