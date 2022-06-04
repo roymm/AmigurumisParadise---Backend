dbe439e3c8a410c68270468c85e0e5bb79214177
roymm <royamunoz@protonmail.com>
2022-06-03 21:15:43 -0600

    Creates a template for all the responses in the system

---
+++ responseAPI.js
@@ -0,0 +1,37 @@
+const e = require("express");
+
+exports.success = (message, results = {}, statusCode) => {
+  return {
+    message,
+    error: false,
+    code: statusCode,
+    results,
+  };
+};
+
+exports.error = (message, results, statusCode) => {
+  // List of common HTTP request code
+  const codes = [200, 201, 400, 401, 404, 403, 422, 500];
+
+  // Get matched code
+  const findCode = codes.find((code) => code == statusCode);
+
+  if (!findCode) statusCode = 500;
+  else statusCode = findCode;
+
+  return {
+    message,
+    code: statusCode,
+    error: true,
+    results: results,
+  };
+};
+
+exports.validation = (errors) => {
+  return {
+    message: "Validation errors",
+    error: true,
+    code: 422,
+    errors,
+  };
+};
