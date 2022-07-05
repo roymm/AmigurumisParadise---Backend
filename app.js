6d56acb53c7e13e6be726eb59ee7ec137e556384
roymm <royamunoz@protonmail.com>
2022-07-05 07:15:48 -0700

    Adds the product routes to the server and uses CORS

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const dotenv = require("dotenv");
 const {mongoose} = require("mongoose");
 const express = require("express");
+const cors = require("cors");
+
 const usersRoutes = require("./routes/users");
+const productRoutes = require("./routes/products")
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 dotenv.config();
 
 mongoose.connect(config.MONGODB_URI)
@@ -19,5 +24,6 @@ mongoose.connect(config.MONGODB_URI)
 
 //Mount routes
 app.use("/api/users", usersRoutes);
+app.use("/api/products", productRoutes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger');
 
 const server = http.createServer(app);
 
+
 server.listen(config.DEFAULT_PORT, () => {
     logger.info(`The server is running at http://localhost:${config.DEFAULT_PORT}`)
 });
